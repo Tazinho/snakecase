de758ae6b222ecc98c8b6e272ea2465aa74e1b47
Tazinho <malte.grosser@gmail.com>
2017-03-13 02:24:04 +0100

    added to small/big camel case and changed docu

---
+++ R/to_small_camel_case.R
@@ -0,0 +1,29 @@
+#' Functions to convert column names to smallCamelCase
+#'
+#' @param camelcases Character string indicating column names of a data.frame.
+#'
+#' @return Vector of character strings in smallCamelCase
+#'
+#' @author Malte Grosser, \email{malte.grosser@@gmail.com}
+#' @keywords utilities
+#'
+#' @examples
+#' camelCases <- c("smallCamelCase", "BigCamelCase", "mixed_Case", "snake_case", "_camel_case__")
+#' to_small_camel_case(camelCases)
+#' to_small_camel_case(names(iris))
+#' to_small_camel_case(c(".", "_"))
+#' to_small_camel_case(NA)
+#' to_small_camel_case(NULL)
+#' to_small_camel_case(character(0))
+#' to_small_camel_case("   N A")
+#'
+#' @importFrom magrittr "%>%"
+#'
+#' @export
+#'
+to_small_camel_case <- function(snake_case){
+  out <- to_big_camel_case(snake_case)
+  out <- stringr::str_c(stringr::str_sub(out, 1, 1) %>% stringr::str_to_lower(),
+                        stringr::str_sub(out, 2))
+  out
+}
