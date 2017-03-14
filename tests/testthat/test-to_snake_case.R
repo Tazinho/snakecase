20f7acdd9266603abfc0b7b28af3f0e0ec24712f
Tazinho <malte.grosser@gmail.com>
2017-03-14 14:22:59 +0100

    updated first tests for to_snake_case

--- tests/testthat/test-to_snake_case.R
+++ tests/testthat/test-to_snake_case.R
@@ -2,10 +2,19 @@ context("to_snake_case")
 
 test_that("basic usage", {
   
-  camelCases <- c("smallCamelCase", "BigCamelCase", "mixed_Case", "snake_case",
-                  "_camel_case__")
-  snake_cases <- c("small_camel_case", "big_camel_case", "mixed_case", "snake_case",
-                  "camel_case")
-  expect_equal(to_snake_case(camelCases), snake_cases)
+  examples <- c(NA,
+                "snake_case",
+                "snakeCase",
+                "SnakeCase",
+                "snake_Case")
+  
+  snake_case <- c("NA",
+                  "snake_case",
+                  "snake_case",
+                  "snake_case",
+                  "snake_case")
+  
+  expect_equal(to_snake_case(examples),
+               snake_case)
 
 })
