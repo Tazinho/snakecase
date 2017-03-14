38caee66227c9c5a96414967ead330b23df3dad9
Tazinho <malte.grosser@gmail.com>
2017-03-14 14:31:37 +0100

    updated first tests for to_small_camel_case

--- tests/testthat/test-to_small_camel_case.R
+++ tests/testthat/test-to_small_camel_case.R
@@ -2,10 +2,19 @@ context("to_small_camel_case")
 
 test_that("basic usage", {
   
-  camelCases <- c("smallCamelCase", "BigCamelCase", "mixed_Case", "snake_case",
-                  "_camel_case__")
-  small_camel_cases <- c("smallCamelCase", "bigCamelCase", "mixedCase", "snakeCase",
-                   "camelCase")
-  expect_equal(to_small_camel_case(camelCases), small_camel_cases)
+  examples <- c(NA,
+                "snake_case",
+                "snakeCase",
+                "SnakeCase",
+                "snake_Case")
+  
+  small_camel_case <- c(NA, 
+                        "snakeCase",
+                        "snakeCase",
+                        "snakeCase",
+                        "snakeCase")
+  
+  expect_equal(to_small_camel_case(examples),
+               small_camel_case)
   
 })
