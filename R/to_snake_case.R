7b395df8dede36f709518af90c93e5f8db0d7ffe
Tazinho <malte.grosser@gmail.com>
2017-03-16 21:39:07 +0100

    added input checking. Input must be a character or NA. Closes #6

--- R/to_snake_case.R
+++ R/to_snake_case.R
@@ -22,6 +22,9 @@
 #' @export
 #'
 to_snake_case <- function(string){
+  # check for valid input
+  if(missing(string)){"argument string is missing, with no default"}
+  if(!(is.character(string)|all(is.na(string) == TRUE))){"argument string has to be an atomic of type character"}
   # catch some input that should be handled like underscores too
   string <- stringr::str_replace_all(string, "\\s+|\\.+", "_")
   # Changes behaviour of the function. Cases like RStudio will be converted
