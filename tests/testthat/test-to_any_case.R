b2ecb5e495dadd6b437f479b086af8382bf744d1
Tazinho <malte.grosser@gmail.com>
2017-03-23 20:29:19 +0100

    default tests for to any case

---
+++ tests/testthat/test-to_any_case.R
@@ -0,0 +1,35 @@
+context("to_any_case")
+
+test_that("examples", {
+  examples <- cases[["examples"]]
+  
+  expect_equal(to_any_case(examples, case = "snake"),
+               cases[["snake_case"]])
+  
+  expect_equal(to_any_case(examples, case = "small_camel"),
+               cases[["small_camel_case"]])
+  
+  expect_equal(to_any_case(examples, case = "big_camel"),
+               cases[["big_camel_case"]])
+  
+  expect_equal(to_any_case(examples, case = "screaming_snake"),
+               cases[["screaming_snake_case"]])
+  }
+)
+
+# test_that("rules",{
+#   examples <- cases[["examples"]]
+#   
+#   expect_equal(to_snake_case(to_snake_case(examples)),
+#                to_snake_case(examples)
+#   ) 
+#   expect_equal(to_snake_case(to_small_camel_case(examples)),
+#                to_snake_case(examples)
+#   ) 
+#   expect_equal(to_snake_case(to_big_camel_case(examples)),
+#                to_snake_case(examples)
+#   ) 
+#   expect_equal(to_snake_case(to_screaming_snake_case(examples)),
+#                to_snake_case(examples)
+#   ) 
+# })
