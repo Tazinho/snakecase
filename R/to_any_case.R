09d8550fd40ee5e594b83bc8cfe9c2da54316c06
Tazinho <malte.grosser@gmail.com>
2017-03-27 23:02:23 +0200

    protect...

--- R/to_any_case.R
+++ R/to_any_case.R
@@ -120,8 +120,8 @@ to_any_case <- function(string, case = c("snake", "small_camel", "big_camel", "s
     protect <- stringr::str_c("[", protect, "]")
     infront <- stringr::str_c("(_+)", protect)
     behind <- stringr::str_c(protect, "(_+)")
-    string <- stringr::str_replace_all(string, infront, "\\1") #%>% 
-  #    stringr::str_replace_all(behind, "\\1")
+    string <- stringr::str_replace_all(string, infront, "\\1") %>% 
+      stringr::str_replace_all(behind, "\\1")
   }
   ## pre and postfix
   string <- stringr::str_c(prefix, string, postfix)
