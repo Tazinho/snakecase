d08ea780ad02fd63cced1da08fad4ea9aa047652
Tazinho <malte.grosser@gmail.com>
2018-08-07 12:28:31 +0200

    add tests

--- tests/testthat/test-to_sentence_case.R
+++ tests/testthat/test-to_sentence_case.R
@@ -5,4 +5,15 @@ test_that("basic_example",{
   expect_equal(
     to_sentence_case("bla bla_bal"),
     "Bla bla bal")
+  
+  expect_equal(to_sentence_case("the_boy_likes_snake_case",
+                                transliterations = c("boy" = "baby",
+                                                     "snake" = "screaming__snake"),
+                                sep_out = " "),
+               "The baby likes screaming snake case")
+  
+  expect_equal(to_sentence_case("the_boy_likes_snake_case",
+                       transliterations = c("boy" = "baby",
+                                            "snake" = "screaming__snake")),
+    "The baby likes screaming snake case")
 })
