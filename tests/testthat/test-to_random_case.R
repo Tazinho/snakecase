3299c4bcc08b7c5a0292eba35eb6bad69197aae9
implexis-analytics <malte.grosser@implexis-analytics.com>
2019-05-13 00:24:03 +0200

    bla

--- tests/testthat/test-to_random_case.R
+++ tests/testthat/test-to_random_case.R
@@ -1,6 +1,6 @@
 test_that("random_case", {
   expect_equal(
-    {RNGversion("3.1");set.seed(123); to_random_case("almost RANDOM")},
+    {RNGversion("3.1"); set.seed(123); to_random_case("almost RANDOM")},
     "AlMosT raNdOm"
   )
 })
